3c2611eaac4a16fedd9cc58c5da4636934420761
Miguel Garcia <miguel.garcia.garcia1201@gmail.com>
2023-01-29 21:52:39 -0600

    Ejercicio Tarea 3 Parcial 1 Calculadora

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int operationIndex = 0;
+
+        do {
+            String operation = args[operationIndex];
+            int parameter1 = Integer.parseInt(args[operationIndex + 1]);
+            int parameter2 = Integer.parseInt(args[operationIndex + 2]);
+
+            switch (operation) {
+                case "SUMA" ->
+                        System.out.println(parameter1 + "+" + parameter2 + "=" + (parameter1 + parameter2));
+                case "RESTA" ->
+                        System.out.println(parameter1 + "-" + parameter2 + "=" + (parameter1 - parameter2));
+                case "MULTIPLICATION" ->
+                        System.out.println(parameter1 + "*" + parameter2 + "=" + (parameter1 * parameter2));
+                case "DIVISION" ->
+                        System.out.println(parameter1 + "/" + parameter2 + "=" + (parameter1 / parameter2));
+            }
+            operationIndex += 3;
+        }while (operationIndex < args.length);
+
+    }
+}
